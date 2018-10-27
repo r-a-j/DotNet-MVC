c2b5a6dfbe1e6926ffba4a2ab0dd0d2db4480e5c
Raj Pawar <er.rajpawar@gmail.com>
2018-10-27 14:42:04 +0530

    ViewData and ViewBag
    View data and view bag concept added in the files
    update in the curd operations

---
+++ ViewDataViewBag/ViewDataViewBag/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ViewDataViewBag.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Action1()
+        {
+            ViewData["viewdata1"] = DateTime.Now.ToString();
+            ViewBag.viewbag1 = DateTime.Now.ToString();
+            return View();
+        }
+
+        
+    }
+}
---
+++ ViewDataViewBag/ViewDataViewBag/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ViewDataViewBag.MvcApplication" Language="C#" %>
