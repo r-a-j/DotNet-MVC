ee26ecf74cdd04db8725c58cb6d44bde79e6141c
Raj Pawar <er.rajpawar@gmail.com>
2018-10-29 14:53:41 +0530

    Partial Views
    Partial views - 2 types\
    i - Static
    ii - Dynamic
    
    The two methods one return void and other return the MvcHtmlString

---
+++ PartialViews_in_MVC/PartialViews_in_MVC/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PartialViews_in_MVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }               
+
+        public PartialViewResult Category()
+        {
+            string[] books = { "PHP", "Java", "C++", "Android" };
+            return PartialView("_CateogryDynamic", books);
+        }
+    }
+}
